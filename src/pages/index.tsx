4c4eb328e3cc836d34d0054d625b3f7cc5cd9408
robertcunningham <robertcunningham@ford.com>
2022-06-11 17:01:18 -0700

    indicate development on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import CheckoutForm from "../components/CheckoutForm";
 const Index = () => (
 	<Layout>
 		<h1>👨‍💻 bagpyp</h1>
+		<p>
+			on branch <code>develop</code>
+		</p>
 		<CheckoutForm />
 	</Layout>
 );

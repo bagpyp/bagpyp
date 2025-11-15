b2d11bdac5bd865c69fa76dc811e0ce116cf8be0
Robert Cunningham <robbie@book-3.local>
2025-11-15 14:49:54 -0800

    g-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from "next/app";
 import "../styles/global.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function _app({ Component, pageProps }: AppProps) {
 	return (
 		<UserProvider>
 			<Component {...pageProps} />
+			{process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+				<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
+			)}
 		</UserProvider>
 	);
 }
